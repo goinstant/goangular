407d283ab5ffa2385abf4f621580ba97944a16dd
Matthew Creager <mattcreager@goinstant.com>
2014-02-05 14:05:25 -0400

    chore(model): Refactored to enable sync replacement

--- examples/js/example.js
+++ examples/js/example.js
@@ -17,24 +17,31 @@ require('goangular');
 var ourCoolApp = angular.module('ourCoolApp', ['goangular']);
 
 ourCoolApp.config(function($goConnectionProvider) {
-  $goConnectionProvider.$set(CONFIG.connectUrl, { user: 'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpc3MiOiJodHRwczovL2dvaW5zdGFudC5uZXQvbWF0dGNyZWFnZXIvRGluZ0RvbmciLCJzdWIiOiJuYSIsImRuIjoiZHVkZSIsImciOltdLCJhdWQiOiJnb2luc3RhbnQubmV0In0.LcvRSDmEj9LHonPhnXv1OWTQ5gjcBnk1QgxH4iYt6PM'});
+  $goConnectionProvider.$set(CONFIG.connectUrl);
 });
 
-ourCoolApp.controller('sweetController', function($scope, $goKey) {
+ourCoolApp.controller('sweetController', function($scope, $goKey, $goQuery, $goConnection) {
 
-  $scope.todos = $goKey('todos');
-  $scope.todos.$sync();
+  //$scope.todos = $goQuery('todos', {}, { sort: { 'timestamp': 'desc' }, limit: 50 }).$sync();
+  $scope.todos = $goKey('todos').$sync();
 
   console.log('todos', $scope.todos);
 
-  // // reference nested key via todos
-  $scope.name = $scope.todos.$key('9edd3cf39e22da900707d9091f90eb52').$key('description').$sync();
+  // //$scope.todos.$sync();
 
-  // // reference nested key via .key
-  $scope.description = $goKey('todos/9edd3cf39e22da900707d9091f90eb52').$sync();
+  // console.log('todos', $scope.todos);
+
+  // // // reference nested key via todos
+  // $scope.name = $scope.todos.$key('9edd3cf39e22da900707d9091f90eb52').$key('description').$sync();
+
+  // // // reference nested key via .key
+  // $scope.description = $goKey('todos/9edd3cf39e22da900707d9091f90eb52').$sync();
 
   setTimeout(function() {
-    $scope.todos.$key('9edd3cf39e22da900707d9091f90eb52/description').$set('jygjygjguj');
+    $scope.todos.$key('9edd3cf39e22da900707d9091f90eb52').$set({
+      timestamp: new Date().getTime(),
+      description: 'an Item'
+    });
   }, 1000);
 
   $scope.addTodo = function() {
@@ -42,13 +49,25 @@ ourCoolApp.controller('sweetController', function($scope, $goKey) {
       timestamp: new Date().getTime(),
       description: $scope.newTodo,
       complete: false
-    }, { expire: 500 }).then(function() {
+    }).then(function() {
       console.log(arguments);
     });
   };
 
   $scope.remove = function(key) {
+    console.log('key remove is called on', key);
     $scope.todos.$key(key).$remove();
   };
 
 });
+
+function whatever(key, room, filter, options) {
+  console.log(arguments)
+
+  var a = new Args([
+    { key: Args.OBJECT | Args.STRING | Args.Required },
+    { room: Args.STRING | Args.Optional },
+    { filter: Args.OBJECT | Args.Optional, _default: {} },
+    { options: Args.OBJECT | Args.Required }
+  ], arguments);
+}

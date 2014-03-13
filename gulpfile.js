cf322fafebf94bd421e3ec59bc6e6814fff2b2bf
Matt Creager <mattcreager@goinstant.com>
2014-03-13 10:23:43 -0300

    chore(gulp): Adding gulp tasks

--- gulpfile.js
+++ gulpfile.js
@@ -1,30 +1,76 @@
+/*jshint node:true*/
 
+'use strict';
 
+var Q = require('q');
+var harp = require('harp');
 var gulp = require('gulp');
-var karma = require('gulp-karma');
+var gulpLoadPlugins = require('gulp-load-plugins');
+var plugins = gulpLoadPlugins();
 
-var testFiles = [
-  'tests/*.js',
-];
+var pathTo = {
+  clean: ['build/', 'dist/'],
+  watch: ['lib/**.js', 'index.js'],
+  tests: ['tests/*.js'],
+  entry: 'index.js',
+  build: 'build/',
+  dist: 'dist/'
+};
+
+gulp.task('clean', function() {
+  gulp.src(pathTo.clean, { read: false }).pipe(plugins.clean({ force: true }));
+});
+
+gulp.task('lint', function() {
+  gulp.src(pathTo.watch)
+    .pipe(plugins.jshint('.jshintrc'))
+    .pipe(plugins.jshint.reporter('jshint-stylish'))
+    .pipe(gulp.dest(pathTo.build));
+});
+
+gulp.task('develop', ['clean'], function() {
+  var deferred = Q.defer();
+
+  gulp.src(pathTo.entry)
+    .pipe(plugins.browserify())
+    .pipe(plugins.rename('build.js'))
+    .pipe(gulp.dest(pathTo.build))
+    .on('end', deferred.resolve);
+
+  return deferred.promise;
+});
+
+gulp.task('build:prod', ['clean', 'develop'], function() {
+    gulp.src(pathTo.entry)
+    .pipe(plugins.browserify())
+    .pipe(plugins.rename('goangular.js'))
+    .pipe(gulp.dest(pathTo.dist))
+    .pipe(plugins.ngmin())
+    .pipe(plugins.rename('goangular.min.js'))
+    .pipe(gulp.dest(pathTo.dist));
+});
 
 gulp.task('test', function() {
-  // Be sure to return the stream
-  return gulp.src(testFiles)
-    .pipe(karma({
+  return gulp.src(pathTo.tests)
+    .pipe(plugins.karma({
       configFile: 'karma.conf.js',
       browsers: ['PhantomJS'],
       action: 'run'
     }))
-    .on('error', function(err) {
-      // Make sure failed tests cause gulp to exit non-zero
-      throw err;
-    });
+    .on('error', throwErr);
 });
 
-gulp.task('default', function() {
-  gulp.src(testFiles)
-    .pipe(karma({
-      configFile: 'karma.conf.js',
-      action: 'watch'
-    }));
+gulp.task('default', ['clean', 'develop'], function() {
+  harp.server(__dirname, { port: process.env.PORT || 5000 });
+
+  var livereload = plugins.livereload();
+  gulp.watch(pathTo.watch).on('change', function(file) {
+    gulp.start('develop', function() {
+      livereload.changed(file.path);
+    });
+  });
 });
+
+function throwErr(err) {
+  throw err;
+}
